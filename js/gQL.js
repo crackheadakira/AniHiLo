9a6f2adff42e2ebbfa435ff4c19ed02a5be9b789
crackheadakira <lasn.mine@gmail.com>
2022-05-02 21:12:23 +0200

    Now cycles through dataset

--- js/gQL.js
+++ js/gQL.js
@@ -1,49 +1,6 @@
 import axios from 'axios';
 
-let query = `
-query($page: Int, $perPage: Int){
-    Page(page: $page, perPage: $perPage) {
-      pageInfo {
-        currentPage
-      }
-      media(format: TV, sort: POPULARITY_DESC, status_not: NOT_YET_RELEASED) {
-        popularity
-        averageScore
-        siteUrl
-        favourites
-        id
-        coverImage {
-          extraLarge
-        }
-        title {
-          native
-          english
-          romaji
-        }
-        startDate {
-          day
-          month
-          year
-        }
-      }
-    }
-  }
-`;
-
-let s1 = getRandomArbitrary(0, 50)
-let s2 = getRandomArbitrary(0, 50)
-let x2 = getRandomArbitrary(0, 20)
-
-let vars = {
-  page: x2,
-  perPage: 50,
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function GQL(query, vars, url = "https://graphql.anilist.co") {
+function getGQL(query, vars, url = "https://graphql.anilist.co") {
   return new Promise((resolve, reject) => {
     axios.post(url, {
       query,
@@ -56,28 +13,5 @@ function GQL(query, vars, url = "https://graphql.anilist.co") {
   });
 };
 
-GQL(query, vars)
-  .then((response, headers) => {
 
-    let data = response.Page;
-
-    let series1 = data.media[s1];
-    let series2 = data.media[s2];
-
-    console.log(series1);
-
-    let series1Title = document.getElementById('seriesTitle1')
-    let series1Img = document.getElementById('seriesImage1')
-    let series1gmValue = document.getElementById('gmValue')
-
-    let series2Title = document.getElementById('seriesTitle2')
-    let series2Img = document.getElementById('seriesImage2')
-
-
-    series1Title.textContent = series1.title.romaji || series1.title.english || series1.title.native;
-    series1Img.src = series1.coverImage.extraLarge;
-    series1gmValue.textContent = `Popularity: ${series1.popularity}`
-
-    series2Title.textContent = series2.title.romaji || series2.title.english || series1.title.native;
-    series2Img.src = series2.coverImage.extraLarge;
-  });
+export default getGQL;
--- js/gameplay.js
+++ js/gameplay.js
@@ -1,11 +1,91 @@
-let series1Title = document.getElementById('seriesTitle1')
-let series1Img = document.getElementById('seriesImage1')
+import getGQL from "./gQL";
 
-let series2Title = document.getElementById('seriesTitle2')
-let series2Img = document.getElementById('seriesImage2')
+let series = [];
+let gamemode = "Popularity";
+let originalData;
 
-series1Title.textContent = series1.title.romaji || series1.title.english;
-series1Img.src = series1.coverImage.extraLarge;
+let query = `
+query($page: Int, $perPage: Int){
+    Page(page: $page, perPage: $perPage) {
+      pageInfo {
+        currentPage
+      }
+      media(format: TV, sort: POPULARITY_DESC, status_not: NOT_YET_RELEASED) {
+        popularity
+        averageScore
+        siteUrl
+        favourites
+        id
+        coverImage {
+          extraLarge
+        }
+        title {
+          native
+          english
+          romaji
+        }
+        startDate {
+          day
+          month
+          year
+        }
+      }
+    }
+  }
+`;
 
-series2Title.textContent = series2.title.romaji || series2.title.english;
-series2Img.src = series2.coverImage.extraLarge;
+let vars = {
+    page: getRandomArbitrary(0, 20),
+    perPage: 50,
+}
+
+let alData = (await getGQL(query, vars)).Page;
+
+function makeSeries() {
+    let series1 = alData.media[getRandomArbitrary(0, 50)];
+    let series2 = alData.media[getRandomArbitrary(0, 50)];
+    series = series.concat(series1, series2)
+    originalData = series;
+}
+
+makeSeries()
+
+setSeries(1)
+setSeries(2)
+
+let higherBTN = document.getElementById('higher');
+higherBTN.addEventListener('click', (e) => {
+    replaceSeries()
+})
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getSeries(series_id) {
+    return series[+series_id - 1]
+}
+
+function replaceSeries() {
+    series1 = originalData[1];
+    series2 = alData.media[getRandomArbitrary(0, 50)]
+    series = [series1, series2]
+    setSeries(1)
+    setSeries(2)
+    makeSeries()
+}
+
+function setSeries(series_id) {
+
+    let series = getSeries(series_id)
+    let seriesT = document.getElementById(`seriesTitle${series_id}`);
+    let seriesI = document.getElementById(`seriesImage${series_id}`);
+    if (series_id == 1) {
+        let gmValue = document.getElementById(`gmValue${series_id}`);
+        gmValue.textContent = `${gamemode}: ${series.popularity}`;
+    }
+
+    seriesT.textContent = series.title.romaji || series.title.english || series.title.native;
+    seriesI.src = series.coverImage.extraLarge;
+
+}
