b11f7ce4062d5a5033b2075647f119b95132de28
crackheadakira <lasn.mine@gmail.com>
2022-05-03 15:34:32 +0200

    Add latest version for merge

--- js/gameplay.js
+++ js/gameplay.js
@@ -3,6 +3,16 @@ import getGQL from "./gQL";
 let series = [];
 let gamemode = "Popularity";
 let originalData;
+let score = document.getElementById('score');
+let scoreValue = 0;
+score.textContent = `Score: ${scoreValue}`
+let series1;
+let series2;
+let series1_spot = getRandomArbitrary(0, 50);
+let series2_spot = getRandomArbitrary(0, 50);
+
+let higherBTN = document.getElementById('higher');
+let odometer = document.querySelector('.odometer');
 
 let query = `
 query($page: Int, $perPage: Int){
@@ -41,23 +51,30 @@ let vars = {
 
 let alData = (await getGQL(query, vars)).Page;
 
-function makeSeries() {
-    let series1 = alData.media[getRandomArbitrary(0, 50)];
-    let series2 = alData.media[getRandomArbitrary(0, 50)];
+fetchSeries()
+setSeries(1)
+setSeries(2)
+
+function fetchSeries() {
+    series1 = alData.media[series1_spot];
+    series2 = alData.media[series2_spot];
     series = series.concat(series1, series2)
     originalData = series;
 }
 
-makeSeries()
-
-setSeries(1)
-setSeries(2)
-
-let higherBTN = document.getElementById('higher');
 higherBTN.addEventListener('click', (e) => {
     replaceSeries()
+    odometer.innerHTML = series1.popularity;
+    console.log(series1)
+    if (series1.popularity < series2.popularity || series1.popularity == series2.popularity) {
+        addScore()
+    }
 })
 
+function addScore() {
+    scoreValue++
+    score.textContent = `Score: ${scoreValue}`;
+}
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
@@ -67,25 +84,26 @@ function getSeries(series_id) {
 }
 
 function replaceSeries() {
+    series2_spot = getRandomArbitrary(0, 50);
     series1 = originalData[1];
-    series2 = alData.media[getRandomArbitrary(0, 50)]
+    series2 = alData.media[series2_spot]
     series = [series1, series2]
     setSeries(1)
     setSeries(2)
-    makeSeries()
+    fetchSeries()
 }
 
 function setSeries(series_id) {
 
     let series = getSeries(series_id)
-    let seriesT = document.getElementById(`seriesTitle${series_id}`);
-    let seriesI = document.getElementById(`seriesImage${series_id}`);
+    let seriesTitle = document.getElementById(`seriesTitle${series_id}`);
+    let seriesImage = document.getElementById(`seriesImage${series_id}`);
     if (series_id == 1) {
         let gmValue = document.getElementById(`gmValue${series_id}`);
         gmValue.textContent = `${gamemode}: ${series.popularity}`;
     }
 
-    seriesT.textContent = series.title.romaji || series.title.english || series.title.native;
-    seriesI.src = series.coverImage.extraLarge;
+    seriesTitle.textContent = series.title.romaji || series.title.english || series.title.native;
+    seriesImage.src = series.coverImage.extraLarge;
 
 }
